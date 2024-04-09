9c6baa33d4e93c687d4e43be87174fd7a3e0009d
Christy <christymonthomas1999@gmail.com>
2024-04-09 18:08:21 +0530

    CourseGoal component added

---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,34 @@
+import { FC, type ReactNode } from "react";
+
+type CourseGoalProps = {
+  title: string;
+  children: ReactNode;
+};
+
+// export default function CourseGoal({ title, children }: CourseGoalProps) {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         {children}
+//       </div>
+//       <button>Delete</button>
+//     </article>
+//   );
+// }
+
+// An alternate to above func decleration
+
+const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        {children}
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+};
+
+export default CourseGoal;
