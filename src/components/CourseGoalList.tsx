fc140ea8c63e9a2f9a98f8245b6040dc36e40c2b
Christy <christymonthomas1999@gmail.com>
2024-04-11 13:05:11 +0530

    Added InfoBox Component

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from "./CourseGoal.tsx";
 import { CourseGoal as CGoal } from "../App.tsx";
+import InfoBox from "./InfoBox.tsx";
+import { type ReactNode } from "react";
 
 type CourseGoalList = {
   goals: CGoal[];
@@ -10,15 +12,33 @@ export default function CourseGoalList({
   goals,
   onDeleteGoal,
 }: CourseGoalList) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">You have no course yet. Start adding some.</InfoBox>
+    );
+  }
+  let warningBox: ReactNode;
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning" severity="high">
+        {" "}
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
